c1a644fdfeb8324321439412a021ef332e4a6889
fulleni <fulleni@hotmail.com>
2025-03-27 05:56:15 +0100

    feat(firebase): export firebase auth implementation

--- lib/ht_authentication_firebase.dart
+++ lib/ht_authentication_firebase.dart
@@ -1,6 +1,4 @@
 /// Firebase implementation of the `ht_authentication_client` interface.
 library;
 
-export 'package:ht_authentication_client/ht_authentication_client.dart';
-
 export 'src/ht_authentication_firebase.dart';
