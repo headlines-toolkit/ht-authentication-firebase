41be0ebb32950b734c7e60310a6866978ca9a8fc
fulleni <fulleni@hotmail.com>
2025-03-27 06:07:02 +0100

    feat(firebase): expose ActionCodeSettings
    - Email link sign-in configuration

--- lib/ht_authentication_firebase.dart
+++ lib/ht_authentication_firebase.dart
@@ -1,4 +1,7 @@
 /// Firebase implementation of the `ht_authentication_client` interface.
 library;
 
+/// Required to configure email link sign-in behavior from within the main app.
+export 'package:firebase_auth/firebase_auth.dart' show ActionCodeSettings;
+
 export 'src/ht_authentication_firebase.dart';
